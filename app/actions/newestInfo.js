883243ed7aa3831baffe9f1ac1e0bad559aa8f41
taichiyi <taichiyi@foxmail.com>
2018-03-01 13:45:31 +0800

    up

---
+++ app/actions/newestInfo.js
@@ -0,0 +1,35 @@
+import * as types from '../constants/ActionTypes';
+
+export function requestNewestInfo(
+  isRefreshing,
+  loading,
+  typeId,
+  isLoadMore,
+  page = 1
+) {
+  return {
+    type: types.REQUEST_ARTICLE_LIST,
+    isRefreshing,
+    loading,
+    isLoadMore,
+    typeId,
+    page
+  };
+}
+
+export function fetchNewestInfo(isRefreshing, loading, isLoadMore = false) {
+  return {
+    type: types.FETCH_ARTICLE_LIST,
+    isRefreshing,
+    loading,
+    isLoadMore
+  };
+}
+
+export function receiveNewestInfo(articleList, typeId) {
+  return {
+    type: types.RECEIVE_ARTICLE_LIST,
+    articleList,
+    typeId
+  };
+}
