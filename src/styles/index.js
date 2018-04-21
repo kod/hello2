f5d8203ad68ea7a79a399122a64faafa2f4e9652
taichiyi <taichiyi@foxmail.com>
2018-04-21 14:38:04 +0800

    refactor(*): 添加样式默认值，部分默认值写活

--- src/styles/index.js
+++ src/styles/index.js
@@ -19,6 +19,10 @@ const globalStyles = StyleSheet.create({
     elevation: 0,
     backgroundColor: globalStyleVariables.HEADER_BACKGROUND_COLOR,
   },
+  scrollView: {
+    height: globalStyleVariables.WINDOW_HEIGHT - globalStyleVariables.APPBAR_HEIGHT - globalStyleVariables.STATUSBAR_HEIGHT + 1,
+    backgroundColor: '#fff',
+  },
 });
 
 export { globalStyles, globalStyleVariables };
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,4 +1,4 @@
-import { Dimensions, Platform } from 'react-native';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 export const BACKGROUND_COLOR = '#f5f5f5';
 export const HEADER_BACKGROUND_COLOR = '#fff';
@@ -8,8 +8,9 @@ export const HIGHLIGHT_COLOR = 'green';
 
 export const WINDOW_WIDTH = Math.floor(Dimensions.get('window').width);
 export const WINDOW_HEIGHT = Math.floor(Dimensions.get('window').height);
+export const SIDEINTERVAL = Math.floor(Dimensions.get('window').width) * 0.04;
 
-export const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 50;
-export const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
+export const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 44;
+export const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 // export const DRAWER_WIDTH =
 //   WINDOW_WIDTH - (Platform.OS === 'android' ? 56 : 64);
