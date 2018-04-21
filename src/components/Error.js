6a9a75ef2075a76164cb4d7c5cb3c8b1f7d32b91
taichiyi <taichiyi@foxmail.com>
2018-04-21 14:36:44 +0800

    refactor(*): 新增组件

---
+++ src/components/Error.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image, TextInput } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { globalStyleVariables } from '../styles';
+
+const styles = StyleSheet.create({
+  tipsWrap: {
+    height: 24,
+  },
+  tips: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: globalStyleVariables.SIDEINTERVAL,
+    paddingRight: globalStyleVariables.SIDEINTERVAL
+  },
+  tipsIcon: {
+    color: '#FD5147',
+    fontSize: 12,
+    marginRight: globalStyleVariables.WINDOW_WIDTH * 0.01,
+    paddingTop: 1
+  },
+  tipsText: {
+    color: '#FD5147',
+    fontSize: 12
+  }
+});
+
+export default ({ style, styleWrap, text, ...restProps }) => {
+  return (
+    <View style={[styles.tipsWrap, styleWrap]}>
+      <View style={[styles.tips, style]}>
+        <MaterialIcons name={'error'} style={styles.tipsIcon} />
+        <Text style={styles.tipsText}>{text}</Text>
+      </View>
+    </View>
+  );
+};
