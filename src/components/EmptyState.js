c90238c6b5589c4973ae7520496e116c0ef4a9e9
taichiyi <taichiyi@foxmail.com>
2018-05-25 09:38:28 +0800

    refactor(*): 空状态页面，组件化

---
+++ src/components/EmptyState.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from '../styles/variables';
+
+const styles = StyleSheet.create({
+  empty: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyImg: {
+    width: WINDOW_WIDTH * 0.5,
+    height: WINDOW_WIDTH * 0.5,
+    marginBottom: WINDOW_HEIGHT * 0.04,
+  },
+  emptyText: {
+    color: '#ccc',
+    fontSize: 12,
+    marginBottom: WINDOW_HEIGHT * 0.1,
+  },
+});
+
+export default ({ style, styleImage, styleText, source, text, ...restProps }) => {
+  return (
+    <View style={[styles.empty, style]} {...restProps} >
+      <Image style={[styles.emptyImg, styleImage]} source={source} />
+      <Text style={[styles.emptyText, styleText]}>{text}</Text>
+    </View>
+  );
+};
