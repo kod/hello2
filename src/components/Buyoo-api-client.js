67a37c726d4736e42216cf7786f6318be00ff953
taichiyi <taichiyi@foxmail.com>
2018-05-25 09:39:06 +0800

    refactor(*): 添加拼单相关接口

--- src/components/Buyoo-api-client.js
+++ src/components/Buyoo-api-client.js
@@ -293,6 +293,74 @@ class ReactStore {
     return this.requestUrl(`:8185/fun/commodity/merge/getInfo`, options);
   }
 
+  mergeGetDetail(options) {
+    if (!options) {
+      return Promise.reject(new Error("fields required"));
+    }
+
+    const data = qs.stringify(options);
+    options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+    
+    return this.requestUrl(`:8185/fun/commodity/merge/getDetail`, options);
+  }
+
+  mergeGetSlave(options) {
+    if (!options) {
+      return Promise.reject(new Error("fields required"));
+    }
+
+    const data = qs.stringify(options);
+    options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+    
+    return this.requestUrl(`:8185/fun/commodity/merge/getSlave`, options);
+  }
+
+  mergeCheck(options) {
+    if (!options) {
+      return Promise.reject(new Error("fields required"));
+    }
+
+    const data = qs.stringify(options);
+    options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+    
+    return this.requestUrl(`:8185/fun/commodity/merge/check`, options);
+  }
+
+  mergeGetMaster(options) {
+    if (!options) {
+      return Promise.reject(new Error("fields required"));
+    }
+
+    const data = qs.stringify(options);
+    options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+    
+    return this.requestUrl(`:8185/fun/commodity/merge/getMaster`, options);
+  }
+
   getAdverstInfo(options) {
     if (!options) {
       return Promise.reject(new Error("fields required"));
