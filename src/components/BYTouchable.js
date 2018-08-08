79fa346313cf54b888d36022e5457e4904fedb76
taichiyi <taichiyi@foxmail.com>
2018-08-08 19:22:46 +0800

    refactor: 禁用水波纹

--- src/components/BYTouchable.js
+++ src/components/BYTouchable.js
@@ -1,32 +1,62 @@
 import React from 'react';
 import {
   TouchableOpacity,
-  TouchableNativeFeedback,
-  Platform,
-  View,
+  // TouchableNativeFeedback,
+  // Platform,
+  // View,
 } from 'react-native';
 
-const PXTouchable = props => {
-  if (Platform.OS === 'android') {
-    const { style, children, backgroundColor = '#ccc', ...otherProps } = props;
-    return (
-      <TouchableNativeFeedback
-        delayPressIn={0}
-        background={TouchableNativeFeedback.Ripple(backgroundColor, false)}
-        useForeground={TouchableNativeFeedback.canUseNativeForeground()}
-        style={style}
-        {...otherProps}
-      >
-        {children
-          ? <View style={style}>
-              {children}
-            </View>
-          : null}
-      </TouchableNativeFeedback>
-    );
-  }
+const PXTouchable = ({ activeOpacity = 0.7, ...props }) => (
+  // if (Platform.OS === 'android' && Platform.Version >= 21) {
+  //   const { style, children, backgroundColor = '#ccc', ...otherProps } = props;
+  //   return (
+  //     <TouchableNativeFeedback
+  //       delayPressIn={0}
+  //       background={TouchableNativeFeedback.SelectableBackground()}
+  //       // background={TouchableNativeFeedback.Ripple(backgroundColor, false)}
+  //       // useForeground={TouchableNativeFeedback.canUseNativeForeground()}
+  //       style={style}
+  //       {...otherProps}
+  //     >
+  //       {children ? <View style={style}>{children}</View> : null}
+  //     </TouchableNativeFeedback>
+  //   );
+  // }
 
-  return <TouchableOpacity {...props} />;
-};
+  <TouchableOpacity activeOpacity={activeOpacity} {...props} />
+);
 
 export default PXTouchable;
+
+// import React from 'react';
+// import {
+//   TouchableOpacity,
+//   TouchableNativeFeedback,
+//   Platform,
+//   View,
+// } from 'react-native';
+
+// const PXTouchable = props => {
+//   if (Platform.OS === 'android') {
+//     const { style, children, backgroundColor = '#ccc', ...otherProps } = props;
+//     return (
+//       <TouchableNativeFeedback
+//         delayPressIn={0}
+//         background={TouchableNativeFeedback.Ripple(backgroundColor, false)}
+//         useForeground={TouchableNativeFeedback.canUseNativeForeground()}
+//         style={style}
+//         {...otherProps}
+//       >
+//         {children
+//           ? <View style={style}>
+//               {children}
+//             </View>
+//           : null}
+//       </TouchableNativeFeedback>
+//     );
+//   }
+
+//   return <TouchableOpacity {...props} />;
+// };
+
+// export default PXTouchable;
