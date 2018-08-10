cffbb2ca2863f0ce081e1957689c91bcda6700ad
taichiyi <taichiyi@foxmail.com>
2018-08-11 07:52:06 +0800

    style: eslint

--- src/common/sagas/cardQuery.js
+++ src/common/sagas/cardQuery.js
@@ -1,7 +1,8 @@
 import { Platform } from 'react-native';
 import { takeEvery, apply, put, select } from 'redux-saga/effects';
+import moment from 'moment';
 import {
-  cardQueryFetch,
+  // cardQueryFetch,
   cardQueryFetchSuccess,
   cardQueryFetchFailure,
 } from '../actions/cardQuery';
@@ -9,13 +10,13 @@ import { addError } from '../actions/error';
 import buyoo from '../helpers/apiClient';
 import {
   CARD_QUERY,
+  // CARD_QUERY,
 } from '../constants/actionTypes';
 import { encryptMD5, signTypeMD5 } from '../../components/AuthEncrypt';
-import moment from 'moment';
 
 import { getAuthUserFunid } from '../selectors';
 
-export function* cardQueryFetchWatchHandle(action) {
+export function* cardQueryFetchWatchHandle(/* action */) {
   try {
     const funid = yield select(getAuthUserFunid);
 
@@ -25,17 +26,17 @@ export function* cardQueryFetchWatchHandle(action) {
     const charset = 'utf-8';
     const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
     const version = '2.0';
-  
+
     const signType = signTypeMD5(appId, method, charset, Key, true);
 
     const encrypt = encryptMD5(
       [
         {
           key: 'funid',
-          value: funid
+          value: funid,
         },
       ],
-      Key
+      Key,
     );
 
     const response = yield apply(buyoo, buyoo.cardQuery, [
@@ -48,16 +49,15 @@ export function* cardQueryFetchWatchHandle(action) {
         timestamp,
         version,
         funid,
-      }
+      },
     ]);
 
     if (response.code !== 10000) {
       yield put(cardQueryFetchFailure());
       yield put(addError(`msg: ${response.msg}; code: ${response.code}`));
-      return false;
+    } else {
+      yield put(cardQueryFetchSuccess(response.result));
     }
-
-    yield put(cardQueryFetchSuccess(response.result));
   } catch (err) {
     yield put(cardQueryFetchFailure());
     yield put(addError(typeof err === 'string' ? err : err.toString()));
