42f4c90fd21a26567213f91858111468b8aa032f
taichiyi <taichiyi@foxmail.com>
2018-08-14 15:44:01 +0800

    feat: 添加正则

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -18,8 +18,10 @@ export const NAME_EXPR = /^.*\s.*$/; // 越南姓名
 export const IDENTIFICATION_EXPR = /^(\d{9}|\d{12})$/; // 越南身份证
 
 export const HTML_REGEX = /html\/(\w+).html/;
-export const BRANDACT_REGEX = /brandAct=(\d+)/;
+export const BRANDID_REGEX = /brandId=(\d+)/;
 export const CLASSIFYID_REGEX = /classifyId=(\d+)/;
+export const SUBCLASSFYID_REGEX = /subClassfyId=(\d+)/;
+export const THIRDCLASSFYID_REGEX = /thirdClassfyId=(\d+)/;
 
 export const WINDOW_WIDTH = Math.floor(Dimensions.get('window').width);
 export const WINDOW_HEIGHT = Math.floor(Dimensions.get('window').height);
