9c896d667cc8a973698cd3547e7c7c140f9f26cb
taichiyi <taichiyi@foxmail.com>
2018-08-14 08:26:51 +0800

    style: eslint

--- src/common/helpers/priceFormat.js
+++ src/common/helpers/priceFormat.js
@@ -3,23 +3,26 @@
  * @Author   taichiyi
  * @DateTime 2017-11-10
  * @param    {number/string}   price      待格式化的金额
- * @param    {string}          separator  分隔符/分节符, 默认为","
+ * @param    {string}          separator  分隔符/分节符, 默认为"."
  * @param    {number}          interval   间隔位数, 默认为3
  * @return   {string}                     格式化后的金额
  */
-const priceFormat = function(price, separator, interval) {
-  price = price === price ? price + '' : '';
-  separator = separator || ',';
+const priceFormat = (price, separator, interval) => {
+  price = price.toString() || '';
+  separator = separator || '.';
   interval = interval || 3;
   let i;
   let result = '';
-  let price_length = price.length;
-  let quotient = parseInt(price_length / interval);
-  let module = price_length % interval;
+  const priceLength = price.length;
+  const quotient = parseInt(priceLength / interval, 10);
+  const module = priceLength % interval;
 
   if (quotient) {
     for (i = quotient; i > 0; i -= 1) {
-      result = separator + price.slice(module + interval * (i - 1), module + interval * i) + result;
+      result =
+        separator +
+        price.slice(module + interval * (i - 1), module + interval * i) +
+        result;
     }
     if (module === 0) {
       result = result.slice(1);
