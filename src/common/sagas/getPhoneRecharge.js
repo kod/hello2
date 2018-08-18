7049ec564eb600244f776e229300adbb24fd6744
taichiyi <taichiyi@foxmail.com>
2018-08-18 18:40:03 +0800

    style: eslint

--- src/common/sagas/getPhoneRecharge.js
+++ src/common/sagas/getPhoneRecharge.js
@@ -1,23 +1,22 @@
 import { Platform } from 'react-native';
-import { takeEvery, apply, put, select } from 'redux-saga/effects';
-import { NavigationActions } from 'react-navigation';
-import { SCREENS } from '../constants';
-import { getPhoneRechargeFetchSuccess, getPhoneRechargeFetchFailure } from '../actions/getPhoneRecharge';
+import { takeEvery, apply, put } from 'redux-saga/effects';
+import moment from 'moment';
+// import { NavigationActions } from 'react-navigation';
+import {
+  getPhoneRechargeFetchSuccess,
+  getPhoneRechargeFetchFailure,
+} from '../actions/getPhoneRecharge';
 import { prepaidFetch } from '../actions/prepaid';
 import { addError } from '../actions/error';
 import buyoo from '../helpers/apiClient';
 import { GET_PHONE_RECHARGE } from '../constants/actionTypes';
 import priceFormat from '../helpers/priceFormat';
 import { encryptMD5, signTypeMD5 } from '../../components/AuthEncrypt';
-import moment from 'moment';
-
-import NavigatorService from '../../navigations/NavigatorService';
+import i18n from '../helpers/i18n';
 
 export function* getPhoneRechargeFetchWatchHandle(action) {
   try {
-    const {
-      msisdn,
-    } = action.payload;
+    const { msisdn } = action.payload;
 
     const Key = 'commodityKey';
     const appId = Platform.OS === 'ios' ? '1' : '2';
@@ -32,10 +31,10 @@ export function* getPhoneRechargeFetchWatchHandle(action) {
       [
         {
           key: 'msisdn',
-          value: msisdn
+          value: msisdn,
         },
       ],
-      Key
+      Key,
     );
 
     const response = yield apply(buyoo, buyoo.getPhoneRecharge, [
@@ -47,8 +46,8 @@ export function* getPhoneRechargeFetchWatchHandle(action) {
         encrypt,
         timestamp,
         version,
-        msisdn
-      }
+        msisdn,
+      },
     ]);
 
     if (response.code !== 10000) {
@@ -60,12 +59,13 @@ export function* getPhoneRechargeFetchWatchHandle(action) {
     if (!response.result.productInfos) {
       // 不支持直冲
 
-      yield put(prepaidFetch({
-        errorText: 'Số điện thoại này không hỗ trợ nạp tiền',
-        providerIcon: '',
-      }));
+      yield put(
+        prepaidFetch({
+          errorText: i18n.thisPhoneNumberNotSupportCharging,
+          providerIcon: '',
+        }),
+      );
       yield put(getPhoneRechargeFetchFailure());
-      return false;
       // F.vue.recharge_fling = false;
       // F.vue.recharge_result = {};
       // F.vue.recharge_prepaid = F.def_recharge_prepaid;
@@ -73,16 +73,26 @@ export function* getPhoneRechargeFetchWatchHandle(action) {
       // F.vue.recharge_selected = 0;
 
       // F.vue.telNumberError = true;
-      // F.vue.telNumberErrorText = 'Số điện thoại này không hỗ trợ nạp tiền';
+      // F.vue.telNumberErrorText = i18n.thisPhoneNumberNotSupportCharging;
       // F.vue.recharge_invalid = false;
     } else {
       // 支持直冲
-
-      yield put(prepaidFetch({
-        errorText: '',
-        providerIcon: response.result.providerIcon,
-        supCreditCard: response.result.supCreditCard,
-      }));
+      yield put(
+        prepaidFetch({
+          errorText: '',
+          providerIcon: response.result.providerIcon,
+          supCreditCard: response.result.supCreditCard,
+        }),
+      );
+      yield put(
+        getPhoneRechargeFetchSuccess({
+          ...response.result,
+          items: response.result.productInfos.map(val => {
+            val.text = `${priceFormat(val.price)} ₫`;
+            return val;
+          }),
+        }),
+      );
 
       // F.vue.recharge_fling = true;
       // F.vue.recharge_result = ret.result;
@@ -94,43 +104,34 @@ export function* getPhoneRechargeFetchWatchHandle(action) {
       // F.vue.telNumberErrorText = '';
       // F.vue.recharge_invalid = true;
     }
-
-    yield put(
-      getPhoneRechargeFetchSuccess({
-        ...response.result,
-        items: response.result.productInfos.map((val, key) => {
-          val.text = `${priceFormat(val.price)} ₫`;
-          return val;
-        }),
-      })
-    );
   } catch (err) {
     yield put(getPhoneRechargeFetchFailure());
     yield put(addError(typeof err === 'string' ? err : err.toString()));
   }
+  return true;
 }
 
-export function* getPhoneRechargeFetchWatch(res) {
+export function* getPhoneRechargeFetchWatch() {
   yield takeEvery(GET_PHONE_RECHARGE.REQUEST, getPhoneRechargeFetchWatchHandle);
 }
 
-export function* getPhoneRechargeSuccessWatchHandle(action) {
-  try {
-    // const {
-    //   tradeNo,
-    //   orderNo,
-    // } = action.payload;
+// export function* getPhoneRechargeSuccessWatchHandle(action) {
+//   try {
+//     // const {
+//     //   tradeNo,
+//     //   orderNo,
+//     // } = action.payload;
 
-    // yield NavigatorService.navigate(SCREENS.Pay, {
-    //   tradeNo,
-    //   orderNo,
-    // });
+//     // yield NavigatorService.navigate(SCREENS.Pay, {
+//     //   tradeNo,
+//     //   orderNo,
+//     // });
 
-  } catch (err) {
-    
-  }
-}
+//   } catch (err) {
 
-export function* getPhoneRechargeSuccessWatch() {
-  yield takeEvery(GET_PHONE_RECHARGE.SUCCESS, getPhoneRechargeSuccessWatchHandle);
-}
+//   }
+// }
+
+// export function* getPhoneRechargeSuccessWatch() {
+//   yield takeEvery(GET_PHONE_RECHARGE.SUCCESS, getPhoneRechargeSuccessWatchHandle);
+// }
--- src/common/sagas/getProvidersCard.js
+++ src/common/sagas/getProvidersCard.js
@@ -1,21 +1,18 @@
 import { Platform } from 'react-native';
-import { takeEvery, apply, put, select } from 'redux-saga/effects';
-import { NavigationActions } from 'react-navigation';
-import { SCREENS } from '../constants';
-import { getProvidersCardFetchSuccess, getProvidersCardFetchFailure } from '../actions/getProvidersCard';
+import { takeEvery, apply, put } from 'redux-saga/effects';
+import moment from 'moment';
+import {
+  getProvidersCardFetchSuccess,
+  getProvidersCardFetchFailure,
+} from '../actions/getProvidersCard';
 import { getProvidersValueFetch } from '../actions/getProvidersValue';
-import { prepaidFetch } from '../actions/prepaid';
 import { addError } from '../actions/error';
 import buyoo from '../helpers/apiClient';
 import { GET_PROVIDERS_CARD } from '../constants/actionTypes';
 import { PROVIDER_TYPE_MAP } from '../constants';
-import priceFormat from '../helpers/priceFormat';
 import { encryptMD5, signTypeMD5 } from '../../components/AuthEncrypt';
-import moment from 'moment';
 
-import NavigatorService from '../../navigations/NavigatorService';
-
-export function* getProvidersCardFetchWatchHandle(action) {
+export function* getProvidersCardFetchWatchHandle(/* action */) {
   try {
     // const {
     //   msisdn,
@@ -37,7 +34,7 @@ export function* getProvidersCardFetchWatchHandle(action) {
         //   value: msisdn
         // },
       ],
-      Key
+      Key,
     );
 
     const response = yield apply(buyoo, buyoo.getProvidersCard, [
@@ -49,7 +46,7 @@ export function* getProvidersCardFetchWatchHandle(action) {
         encrypt,
         timestamp,
         version,
-      }
+      },
     ]);
 
     if (response.code !== 10000) {
@@ -58,36 +55,37 @@ export function* getProvidersCardFetchWatchHandle(action) {
       return false;
     }
 
-    yield put(
-      getProvidersCardFetchSuccess(response.result)
-    );
+    yield put(getProvidersCardFetchSuccess(response.result));
   } catch (err) {
     yield put(getProvidersCardFetchFailure());
     yield put(addError(typeof err === 'string' ? err : err.toString()));
   }
+  return true;
 }
 
-export function* getProvidersCardFetchWatch(res) {
+export function* getProvidersCardFetchWatch() {
   yield takeEvery(GET_PROVIDERS_CARD.REQUEST, getProvidersCardFetchWatchHandle);
 }
 
 export function* getProvidersCardSuccessWatchHandle(action) {
   try {
     const { items } = action.payload;
-    
+
     yield put(
       getProvidersValueFetch({
         providerName: items[0].providerName,
         providerCode: items[0].providerCode,
-        providerType: PROVIDER_TYPE_MAP['phoneCard'],
-      })  
+        providerType: PROVIDER_TYPE_MAP.phoneCard,
+      }),
     );
-    
   } catch (err) {
     console.log(err);
   }
 }
 
 export function* getProvidersCardSuccessWatch() {
-  yield takeEvery(GET_PROVIDERS_CARD.SUCCESS, getProvidersCardSuccessWatchHandle);
+  yield takeEvery(
+    GET_PROVIDERS_CARD.SUCCESS,
+    getProvidersCardSuccessWatchHandle,
+  );
 }
