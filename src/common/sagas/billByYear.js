abb9798513533b2dd182515b63f84373094f9de5
taichiyi <taichiyi@foxmail.com>
2018-08-24 09:56:12 +0800

    fix(billByYear.js): #178

--- src/common/sagas/billByYear.js
+++ src/common/sagas/billByYear.js
@@ -1,25 +1,25 @@
 import { Platform } from 'react-native';
 import { takeEvery, apply, put, select } from 'redux-saga/effects';
-import { NavigationActions } from 'react-navigation';
-import { SCREENS } from '../constants';
-import { billByYearFetchSuccess, billByYearFetchFailure } from '../actions/billByYear';
-import { billDetailsFetch } from '../actions/billDetails';
+import moment from 'moment';
+// import { NavigationActions } from 'react-navigation';
+// import { SCREENS } from '../constants';
+import {
+  billByYearFetchSuccess,
+  billByYearFetchFailure,
+} from '../actions/billByYear';
+// import { billDetailsFetch } from '../actions/billDetails';
 import { addError } from '../actions/error';
 import buyoo from '../helpers/apiClient';
 import { BILL_BY_YEAR } from '../constants/actionTypes';
 import { encryptMD5, signTypeMD5 } from '../../components/AuthEncrypt';
-import moment from 'moment';
 
 import { getAuthUserFunid } from '../selectors';
 
-import NavigatorService from '../../navigations/NavigatorService';
+// import NavigatorService from '../../navigations/NavigatorService';
 
 export function* billByYearFetchWatchHandle(action) {
   try {
-    const {
-      year,
-      init = false,
-    } = action.payload;
+    const { year, init = false } = action.payload;
     const funid = yield select(getAuthUserFunid);
 
     const Key = 'settleKey';
@@ -35,14 +35,14 @@ export function* billByYearFetchWatchHandle(action) {
       [
         {
           key: 'funid',
-          value: funid
+          value: funid,
         },
         {
           key: 'year',
-          value: year
+          value: year,
         },
       ],
-      Key
+      Key,
     );
 
     const options = [
@@ -56,101 +56,108 @@ export function* billByYearFetchWatchHandle(action) {
         version,
         funid,
         year,
-      }
+      },
     ];
 
     const response = yield apply(buyoo, buyoo.billByYear, options);
+    let { result } = response;
 
-    let result = response.result;
-    
     if (response.code !== 10000) {
       yield put(billByYearFetchFailure());
       yield put(addError(`msg: ${response.msg}; code: ${response.code}`));
-      return false;
+    } else {
+      const lengthNo = () => new Array(12).fill({});
+
+      // const lengthYes = () => {
+      //   let beforeMonth;
+      //   let afterMonth;
+
+      //   if (result.length === 1) {
+      //     beforeMonth = parseInt(result[0].month.toString().slice(4, 6), 10);
+      //     afterMonth = parseInt(result[0].month.toString().slice(4, 6), 10);
+      //   } else {
+      //     beforeMonth = parseInt(result[0].month.toString().slice(4, 6), 10);
+      //     afterMonth = parseInt(
+      //       result[result.length - 1].month.toString().slice(4, 6),
+      //       10,
+      //     );
+      //   }
+
+      //   const beforeNumber = beforeMonth - 1;
+      //   const afterNumber = 12 - afterMonth;
+
+      //   for (let index = 0; index < beforeNumber; index += 1) {
+      //     result.unshift({});
+      //   }
+
+      //   for (let index = 0; index < afterNumber; index += 1) {
+      //     result.push({});
+      //   }
+
+      //   return result;
+      // };
+
+      const lengthYes = () =>
+        new Array(12).fill({}).map((val, index) => {
+          let resultMap = {};
+          result.forEach(val1 => {
+            if (index + 1 === parseInt(val1.month.toString().slice(4, 6), 10)) {
+              resultMap = val1;
+            }
+          });
+          return resultMap;
+        });
+
+      result = result.length === 0 ? lengthNo() : lengthYes();
+
+      const isHaveBill = init ? result.length !== 0 : true;
+
+      let isOverdue = false;
+      result = result.map(val => {
+        if (val.month)
+          val.month = parseInt(val.month.toString().slice(4, 6), 10);
+        if (val.status === 10007) isOverdue = true;
+        return val;
+      });
+
+      yield put(
+        billByYearFetchSuccess({
+          year,
+          result,
+          isOverdue,
+          isHaveBill,
+        }),
+      );
     }
-
-    const lengthNo = () => {
-      return new Array(12).fill({});
-    }
-
-    const lengthYes = result => {
-      let beforeMonth;
-      let afterMonth;
-
-      if (result.length === 1) {
-        beforeMonth = parseInt(result[0].month.toString().slice(4, 6));
-        afterMonth = parseInt(result[0].month.toString().slice(4, 6));
-      } else {
-        beforeMonth = parseInt(result[0].month.toString().slice(4, 6));
-        afterMonth = parseInt(result[result.length - 1].month.toString().slice(4, 6));
-      }
-      
-      const beforeNumber = beforeMonth - 1;
-      const afterNumber = 12 - afterMonth;
-
-      for (let index = 0; index < beforeNumber; index += 1) {
-        result.unshift({});
-      }
-
-      for (let index = 0; index < afterNumber; index += 1) {
-        result.push({});
-      }
-
-      return result;
-    }
-
-    result = result.length === 0 ? lengthNo() : lengthYes(result);
-
-    
-    let isHaveBill = init ? result.length !== 0 : true;
-
-    let isOverdue = false;
-    result = result.map((val, key) => {
-      if (val.month) val.month = parseInt(val.month.toString().slice(4, 6));
-      if (val.status === 10007) isOverdue = true;
-      return val;
-    });
-    
-    yield put(billByYearFetchSuccess({
-      year,
-      result,
-      isOverdue,
-      isHaveBill,
-    }));
   } catch (err) {
     yield put(billByYearFetchFailure());
     yield put(addError(typeof err === 'string' ? err : err.toString()));
   }
 }
 
-export function* billByYearFetchWatch(res) {
+export function* billByYearFetchWatch() {
   yield takeEvery(BILL_BY_YEAR.REQUEST, billByYearFetchWatchHandle);
 }
 
-
-export function* billByYearSuccessWatchHandle(action) {
-  try {
-    const {
-      isHaveBill
-    } = action.payload;
-    if (isHaveBill === false ) {
-      Alert.alert(
-        '',
-        '您还没有产生账单',
-        [
-          { 
-            text: i18n.confirm, 
-            onPress: () => { NavigatorService.pop(1); }
-          }
-        ]
-      )
-    } else {
-      // yield put(billDetailsFetch());
-    }
-
-  } catch (err) {
-    
-  }
+export function* billByYearSuccessWatchHandle() {
+  // try {
+  //   const { isHaveBill } = action.payload;
+  //   if (isHaveBill === false ) {
+  //     yield Alert.alert('',
+  //       i18n.noBill,
+  //       [
+  //         {
+  //           text: i18n.confirm,
+  //           onPress: () => { NavigatorService.pop(1); }
+  //         }
+  //       ]
+  //     )
+  //   } else {
+  //     // yield put(billDetailsFetch());
+  //   }
+  // } catch (err) {
+  //   console.warn(err);
+  // }
 }
 
 export function* billByYearSuccessWatch() {
