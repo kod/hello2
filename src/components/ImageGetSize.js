9144f1d30a021c905daf88acf9426e3429b0a58b
taichiyi <taichiyi@foxmail.com>
2018-08-25 09:53:03 +0800

    style(ImageGetSize.js): eslint

--- src/components/ImageGetSize.js
+++ src/components/ImageGetSize.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import { View, Image, InteractionManager } from 'react-native';
-import { WINDOW_WIDTH, WINDOW_HEIGHT, SIDEINTERVAL, } from '../common/constants';
+import { Image, InteractionManager } from 'react-native';
+import { WINDOW_WIDTH } from '../common/constants';
 
 class ImageGetSize extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        width: 0,
-        height: 0,
+      width: 0,
+      height: 0,
     };
   }
 
   componentDidMount() {
     const { uri } = this.props;
-      Image.getSize(
-        uri,
-        (width, height) => {
-          InteractionManager.runAfterInteractions(() => {
-            this.setState({width, height});
-          });
-        },
-        (error) => {
-          console.dir(error);
-        },
-      )
+    Image.getSize(
+      uri,
+      (width, height) => {
+        InteractionManager.runAfterInteractions(() => {
+          this.setState({ width, height });
+        });
+      },
+      error => {
+        console.dir(error);
+      },
+    );
   }
 
   render() {
     const { width, height } = this.state;
     const { uri, ...restProps } = this.props;
     return (
-      <Image 
-        style={{width: WINDOW_WIDTH, height: height / width * WINDOW_WIDTH, resizeMode: 'contain'}} 
-        source={{uri: uri}}
+      <Image
+        style={{
+          width: WINDOW_WIDTH,
+          height: (height / width) * WINDOW_WIDTH,
+          resizeMode: 'contain',
+        }}
+        source={{ uri }}
         {...restProps}
       />
     );
