671c14a5ef5419e9c65881c2d08e6398d1705b82
taichiyi <taichiyi@foxmail.com>
2018-08-25 09:29:18 +0800

    style: eslint

--- src/components/PhoneAdBaner.js
+++ src/components/PhoneAdBaner.js
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Image, Dimensions } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { withNavigation } from 'react-navigation';
 
-import { WINDOW_WIDTH, SCREENS, SIDEINTERVAL, } from '../common/constants';
+import { WINDOW_WIDTH, SCREENS } from '../common/constants';
 
 import BYTouchable from './BYTouchable';
 
 const styles = StyleSheet.create({
   itemWrap: {
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   itemImg: {
     height: WINDOW_WIDTH * 0.3625,
     width: WINDOW_WIDTH,
     marginBottom: 5,
-    resizeMode: Image.resizeMode.overflow
-  }
+    resizeMode: Image.resizeMode.overflow,
+  },
 });
 
 class PhoneAdBaner extends Component {
   render() {
     const {
       groupon = false,
-      data, 
+      data,
       style,
       navigation: { navigate },
       ...restProps
     } = this.props;
-    
+
     return (
       <View style={[styles.itemWrap, style]} {...restProps}>
         {data.map((val, key) => (
-          <BYTouchable 
-          style={styles.touchable} 
+          <BYTouchable
+            style={styles.touchable}
             backgroundColor="transparent"
-            key={key} 
-            onPress={() => navigate(SCREENS.ProductDetail,  { brandId: val.brandId, groupon })}
+            key={key}
+            onPress={() =>
+              navigate(SCREENS.ProductDetail, { brandId: val.brandId, groupon })
+            }
           >
-            <Image source={{ uri: `${val.imageUrl}?x-oss-process=image/quality,Q_70` }} style={styles.itemImg} />
+            <Image
+              source={{
+                uri: `${val.imageUrl}?x-oss-process=image/quality,Q_70`,
+              }}
+              style={styles.itemImg}
+            />
           </BYTouchable>
         ))}
       </View>
