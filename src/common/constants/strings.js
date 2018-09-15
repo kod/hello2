abf630460282552b5384b81a068cd72dabafd702
taichiyi <taichiyi@foxmail.com>
2018-09-15 16:44:45 +0800

    refactor: 打开多语言

--- src/common/constants/strings.js
+++ src/common/constants/strings.js
@@ -1,14 +1,14 @@
-// import en from './strings/en.json';
+import en from './strings/en.json';
 import vi from './strings/vi.json';
-// import zh from './strings/zh.json';
+import zh from './strings/zh.json';
 // import zhTW from './strings/zh-TW.json';
 // import zhHK from './strings/zh-HK.json';
 // import zhMO from './strings/zh-MO.json';
 
 export default {
-  // en,
+  en,
   vi,
-  // zh,
+  zh,
   // 'zh-TW': zhTW,
   // 'zh-HK': zhHK,
   // 'zh-MO': zhMO,
